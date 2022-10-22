dda1570a2bdb9815465321ca82130dd23440efd0
Oleksii Redko <1chelovechek@ukr.net>
2022-10-22 16:15:27 +0300

    .editorconfig modified3

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,15 @@
 module.exports = {
-  env: {
-    node: true,
-    es2021: true,
-  },
-  extends: ['standard', 'plugin:sonarjs/recommended', 'prettier'],
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['sonarjs'],
+	env: {
+		node: true,
+		es2021: true,
+	},
+	extends: ['standard', 'plugin:sonarjs/recommended', 'prettier'],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['sonarjs'],
 
-  rules: {},
+	rules: {},
 }
