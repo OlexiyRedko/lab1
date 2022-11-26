16dbe7cbaa45c8387f49cf8011e9fbe6397e92b6
Oleksii Redko <1chelovechek@ukr.net>
2022-11-26 06:01:05 +0200

    test 7

--- api/function1.js
+++ api/function1.js
@@ -2,5 +2,6 @@ import server from "../src/server.js";
 
 export default function handler(req, res) {
   // server(req, res)
+  const { name = 'World' } = req.query
   return res.send(`Hello ${name}!`)
 }
